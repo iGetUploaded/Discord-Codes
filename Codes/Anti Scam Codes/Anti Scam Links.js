e16bc2dad2cf64c94282e0fc1318a72f31183dae
Cursed <54633753+iGetUploaded@users.noreply.github.com>
2021-12-15 23:31:45 -0500

    Add files via upload

---
+++ Codes/Anti Scam Codes/Anti Scam Links.js
@@ -0,0 +1,13 @@
+client.on('messageCreate', async message => {
+    const array = require(`./scam.json`)
+    if(array.includes(message.content)) {
+      message.delete()
+      try {
+  
+  client.guilds.cache.get(message.guild.id).members.cache.get(message.author.id).ban({ days: 7, reason: `Steam Scam Link\n(${message.content})` })
+      } catch(e) {
+        console.log(e)
+      }
+      message.channel.send({content: `${message.author} was banned due to being hacked (Steam Scam Link)`})
+    }
+  })
